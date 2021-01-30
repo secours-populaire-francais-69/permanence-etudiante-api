cbdebcbac52a0440bb1e81d9120ad8121624eadd
quentin basset <quentin.basset@querio.com>
2021-01-30 17:56:41 +0100

    :wrench: fix vowfile

--- vowfile.js
+++ vowfile.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -11,7 +11,7 @@
 */
 
 // Uncomment when want to run migrations
-// const ace = require('@adonisjs/ace')
+const ace = require("@adonisjs/ace");
 
 module.exports = (cli, runner) => {
   runner.before(async () => {
@@ -24,7 +24,7 @@ module.exports = (cli, runner) => {
     | line, if http server is not required
     |
     */
-    use('Adonis/Src/Server').listen(process.env.HOST, process.env.PORT)
+    use("Adonis/Src/Server").listen(process.env.HOST, process.env.PORT);
 
     /*
     |--------------------------------------------------------------------------
@@ -34,8 +34,8 @@ module.exports = (cli, runner) => {
     | Migrate the database before starting the tests.
     |
     */
-    // await ace.call('migration:run', {}, { silent: true })
-  })
+    await ace.call("migration:run", {}, { silent: true });
+  });
 
   runner.after(async () => {
     /*
@@ -46,7 +46,7 @@ module.exports = (cli, runner) => {
     | Shutdown the HTTP server when all tests have been executed.
     |
     */
-    use('Adonis/Src/Server').getInstance().close()
+    use("Adonis/Src/Server").getInstance().close();
 
     /*
     |--------------------------------------------------------------------------
@@ -57,6 +57,6 @@ module.exports = (cli, runner) => {
     | original state
     |
     */
-    // await ace.call('migration:reset', {}, { silent: true })
-  })
-}
+    await ace.call("migration:reset", {}, { silent: true });
+  });
+};
