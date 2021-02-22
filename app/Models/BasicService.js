1dfe370add29ec28847f1eaedf96d8aa9fbe33bc
quentin basset <quentin.basset@querio.com>
2021-02-22 23:25:00 +0100

    :sparkles: add model basic_services

---
+++ app/Models/BasicService.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use("Model");
+
+/**
+ *  @swagger
+ *  definitions:
+ *    BasicService:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: number
+ *        startAt:
+ *          type: datetime
+ *        endAt:
+ *          type: datetime
+ *        maxPeople:
+ *          type: number
+ *        isClosed:
+ *          type: boolean
+ *        createdAt:
+ *          type: string
+ *          format: date-time
+ *        updatedAt:
+ *          type: string
+ *          format: date-time
+ *      required:
+ *        - startAt
+ *        - endAt
+ *        - isClosed
+ */
+class BasicService extends Model {
+  static get dates() {
+    return super.dates.concat(["startAt", "endAt"]);
+  }
+}
+
+module.exports = BasicService;
