20b3f1442dc2d187c5441212efae7857927d99c8
quentin basset <quentin.basset@querio.com>
2021-02-23 20:39:46 +0100

    :sparkles: add model for BasicServiceSubscriber

---
+++ app/Models/BasicServiceSubscriber.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const Model = use("Model");
+
+/**
+ *  @swagger
+ *  definitions:
+ *    BasicServiceSubscriber:
+ *      type: object
+ *      properties:
+ *        id:
+ *          type: number
+ *        basicServiceId:
+ *          type: number
+ *        userId:
+ *          type: number
+ *        createdAt:
+ *          type: string
+ *          format: date-time
+ *        updatedAt:
+ *          type: string
+ *          format: date-time
+ *      required:
+ *        - basicServiceId
+ *        - userId
+ */
+class BasicServiceSubscriber extends Model {
+  basicServices() {
+    return this.belongsToMany("App/Models/BasicService");
+  }
+
+  users() {
+    return this.belongsToMany("App/Models/User");
+  }
+}
+
+module.exports = BasicServiceSubscriber;
