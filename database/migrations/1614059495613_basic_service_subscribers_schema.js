6e5e8d89c65d5014c2188d9c8161c43d802aacf7
quentin basset <quentin.basset@querio.com>
2021-02-23 20:39:26 +0100

    :sparkles: add migration file for basic_service_subscribers

---
+++ database/migrations/1614059495613_basic_service_subscribers_schema.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class BasicServiceSubscribersSchema extends Schema {
+  up() {
+    this.create("basic_service_subscribers", (table) => {
+      table.integer("userId").unsigned().references("id").inTable("users");
+      table
+        .integer("basicServiceId")
+        .unsigned()
+        .references("id")
+        .inTable("basic_services");
+      table.increments();
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("basic_service_subscribers");
+  }
+}
+
+module.exports = BasicServiceSubscribersSchema;
